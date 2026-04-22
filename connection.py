fe7843d6ec71c4aa1d9c036d7a8047a335313431
karythonunidesc <karython.oliveira@unidesc.edu.br>
2026-04-22 20:53:19 -0300

    conexao com banco de dados, migrate feita e criacao das regras de validacao

--- connection.py
+++ connection.py
@@ -1,1 +1,26 @@
 # arquivo para configuraçao da conexao com o banco de dados
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.exc import OperationalError
+
+
+load_dotenv()
+
+
+SQLALCHEMY_DATABASE_URI=os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    url=SQLALCHEMY_DATABASE_URI,
+    echo=True,
+    pool_pre_ping=True
+)
+
+Base = declarative_base()
+
+try:
+    with engine.connect() as connection:
+        print("Banco de dados Conectado!")
+except OperationalError as e:
+    print("Falha na conexão!")
--- src/__init__.py
+++ src/__init__.py
@@ -3,12 +3,18 @@
 # configurar a connection para apontar para o confiig object do flask
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
+app.config.from_object('connection')
 
+db = SQLAlchemy(app)
 
-
+migrate = Migrate(app, db)
+ma = Marshmallow(app)
 
 
 
@@ -16,7 +22,7 @@ app = Flask(__name__)
 
 # TODO : Apontar os modelos criados para a orm conseguir criar as tabelas
 
-#from .models.usuario_model import ClassUsuario
+from .models.usuario_model import UsuarioModel
 
 
 # TODO: Apontar quem são as minhas views
