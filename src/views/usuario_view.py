ebd6b3775b16efb376dd6c32ce7a74167befc9e3
karythonunidesc <karython.oliveira@unidesc.edu.br>
2026-05-20 21:08:54 -0300

    aula10 20-05-26, camada de service e estrutura da view

--- src/views/usuario_view.py
+++ src/views/usuario_view.py
@@ -1,1 +1,31 @@
-# criar as rotas da API, camada que recebe as requisicoes
+from flask_restful import Resource
+from marshmallow import ValidationError
+from src.schemas import usuario_schema
+from flask import request, jsonify, make_response
+from src.models.usuario_model import UsuarioModel
+from src.services import usuario_service
+from src import api
+
+
+class UsuarioList(Resource):
+    def get(self):
+        ...
+    
+    def post(self):
+        ...
+
+api.add_resource(UsuarioList, '/usuarios')
+
+
+class UsuarioResource(Resource):
+    def get(self, id_usuario):
+        ...
+
+    def put(self, id_usuario):
+        ...
+
+    def delete(self, id_usuario):
+        ...
+
+
+api.add_resource(UsuarioResource, '/usuarios/<int:id_usuario>')
